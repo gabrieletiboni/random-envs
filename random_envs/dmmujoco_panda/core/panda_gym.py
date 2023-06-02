6e4f61b032971c44153947031f2ea769549b7250
gtiboni@watt <gtiboni@watt>
2023-06-03 00:43:29 +0200

    qacc scaled back to 80%, instead of the low 40% value

--- random_envs/dmmujoco_panda/core/panda_gym.py
+++ random_envs/dmmujoco_panda/core/panda_gym.py
@@ -286,7 +286,7 @@ class PandaGymEnvironment(RandomEnv):
         self.joint_qvel_max = joint_qvel_limits
 
         # qacc limits
-        self.joint_qacc_max = np.array([15, 7.5, 10, 12.5, 15, 20, 20])*0.4
+        self.joint_qacc_max = np.array([15, 7.5, 10, 12.5, 15, 20, 20])*0.8
 
     @property
     def dt(self):
