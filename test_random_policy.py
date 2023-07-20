92c50b6e2cd5ad55b2ae353095382e8f3fe98dcb
gabrieleHP <tibonigabriele@gmail.com>
2023-07-21 00:16:46 +0200

    indentation converted to spaces

--- test_random_policy.py
+++ test_random_policy.py
@@ -1,13 +1,13 @@
 """Random policy on RandomEnvs environments
 
-	Example:
-		(no-rendering)
-		unset LD_PRELOAD; python test_random_policy.py --env RandomHopper-v0
-		unset LD_PRELOAD; python test_random_policy.py --env RandomHopper-v0 --udr 0.5
+    Example:
+        (no-rendering)
+        unset LD_PRELOAD; python test_random_policy.py --env RandomHopper-v0
+        unset LD_PRELOAD; python test_random_policy.py --env RandomHopper-v0 --udr 0.5
 
-		(rendering)
-			export LD_PRELOAD=$CONDA_PREFIX/lib/libGLEW.so; python test_random_policy.py --env RandomHopper-v0 --render
-			NOTE: the libGLEW.so could be located elsewhere (https://github.com/openai/mujoco-py/issues/268)
+        (rendering)
+            export LD_PRELOAD=$CONDA_PREFIX/lib/libGLEW.so; python test_random_policy.py --env RandomHopper-v0 --render
+            NOTE: the libGLEW.so could be located elsewhere (https://github.com/openai/mujoco-py/issues/268)
 """
 import argparse
 import pdb
@@ -17,30 +17,30 @@ import gym
 import random_envs
 
 def main():
-	env = gym.make(args.env)
+    env = gym.make(args.env)
 
-	if args.udr is not None:
-		env.set_dr_distribution(dr_type='uniform', distr=env.get_uniform_dr_by_percentage(percentage=args.udr))
-		env.set_dr_training(True)
+    if args.udr is not None:
+        env.set_dr_distribution(dr_type='uniform', distr=env.get_uniform_dr_by_percentage(percentage=args.udr))
+        env.set_dr_training(True)
 
-	state = env.reset()
-	done = False
+    state = env.reset()
+    done = False
 
-	print('============================')
-	print('Env:', args.env)
-	print('Action space:', env.action_space)
-	print('State space:', env.observation_space)
-	print('Task dim:', env.task_dim)
-	print('Current task:', env.get_task())
+    print('============================')
+    print('Env:', args.env)
+    print('Action space:', env.action_space)
+    print('State space:', env.observation_space)
+    print('Task dim:', env.task_dim)
+    print('Current task:', env.get_task())
 
-	while True:
-		state, reward, done, info = env.step(env.action_space.sample())
-		
-		if args.render:
-			env.render()
+    while True:
+        state, reward, done, info = env.step(env.action_space.sample())
+        
+        if args.render:
+            env.render()
 
-		if done:
-			env.reset()
+        if done:
+            env.reset()
 
 
 def parse_args():
@@ -53,4 +53,4 @@ def parse_args():
 args = parse_args()
 
 if __name__ == '__main__':
-	main()
+    main()
